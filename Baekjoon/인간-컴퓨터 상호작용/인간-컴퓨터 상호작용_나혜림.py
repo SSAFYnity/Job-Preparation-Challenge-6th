0982d4bd1740eac995175f0dd22f8892fac0b0cc
나혜림 <rimhyena@gmail.com>
2025-05-15 16:06:17 +0900

    solved 인간-컴퓨터 상호작용- 300ms 153096kb

--- Baekjoon/인간-컴퓨터 상호작용/인간-컴퓨터 상호작용_나혜림.py
+++ Baekjoon/인간-컴퓨터 상호작용/인간-컴퓨터 상호작용_나혜림.py
@@ -1,54 +1,22 @@
-data = input()
-T = int(input())
-basic = {
-    'a': 0,
-    'b': 0,
-    'c': 0,
-    'd': 0,
-    'e': 0,
-    "f": 0,
-    "g": 0,
-    "h": 0,
-    "i": 0,
-    "j": 0,
-    "k": 0,
-    "l": 0,
-    "m": 0,
-    "n": 0,
-    "o": 0,
-    "p": 0,
-    "q": 0,
-    "r": 0,
-    "s": 0,
-    "t": 0,
-    "u": 0,
-    "v": 0,
-    "w": 0,
-    "x": 0,
-    "y": 0,
-    "z": 0,
-}
+import sys
+input = sys.stdin.readline
 
-apt = list(basic.keys())
-lst = []
-for j in range(0, len(data)):
-    if j == 0:
-        lst.insert(j, basic)
-    else:
-        lst.insert(j, lst[j-1].copy())
-    w = data[j]
-    if w in lst[j].keys():
-        if j != 0:
-            x = lst[j-1][w] + 1
-            lst[j][w] = x
-        else:
-            lst[j][w] += 1
-    # lst[j]에 해당하는 알파벳의 숫자를 그 전 데이터와 비교해서 더해주기
-for i in range(T):
-    S, l, r = input().split()
-    num1 = int(l)
-    num2 = int(r)
-    if data[num1] != S:
-        print(lst[num2][S] - lst[num1][S])
-    else:
-        print(lst[num2][S] - lst[num1][S]+1)
+data = input().strip()
+T = int(input())
+
+# 누적합 배열 초기화: 각 알파벳에 대해 누적합을 저장
+prefix = [[0] * (len(data) + 1) for _ in range(26)]
+
+# 누적합 계산
+for i in range(len(data)):
+    for j in range(26):
+        prefix[j][i+1] = prefix[j][i]  # 이전 값 복사
+    prefix[ord(data[i]) - 97][i+1] += 1  # 해당 알파벳 개수 증가
+
+# 질의 처리
+for _ in range(T):
+    char, l, r = input().split()
+    x = ord(char) - 97
+    l = int(l)
+    r = int(r)
+    print(prefix[x][r+1] - prefix[x][l])
