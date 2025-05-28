9d10a0c00522ff2185845e7f5e66eb8a81d4b208
skyungg <167261107+skyungg@users.noreply.github.com>
2025-05-28 03:48:57 +0000

    solved 마법의엘리베이터 - 708ms 206.228mb

--- Baekjoon/등차수열/등차수열_허승경.java
+++ Baekjoon/등차수열/등차수열_허승경.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 /*
  * (범위 확인) -> arr[i] >= 0 && arr[i] < 10^9 -> int 형 으로 선언
- * 아이디어 : arr[i]를 끝원소로 하는 등차수열  
+ * 아이디어 : dp
+ * -> 
  * ---
  * 초기 아이디어: 이분탐색 -> 아닌듯 => 선형탐색? -> 시간초과 발생
  * 1. 배열에 저장 후, 오름차순 정렬하기
@@ -18,27 +19,26 @@ public class Main {
 		
 		int n = Integer.parseInt(br.readLine());
 		int [] arr = new int[n];
-		Set<Integer> set = new HashSet<>();
 		
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-			set.add(arr[i]);
 		}
 		
 		Arrays.sort(arr); 	// 오름차순 정렬
 		int maxLength = 1;	// 최대 길이 초기화 (n의 최솟값 1)
 		
-		for(int i = 0; i < n-1; i++) {
-			for(int j = i+1; j < n; j++) {
-				int diff = arr[j]-arr[i];		// 앞-뒤 원소의 차
-				int curLength = 2;					// 원소의 길이(현재 2)
-				int nextNum = arr[j]+diff;		// 다음 원소 값
+		Map<Integer, Integer> [] dp = new HashMap[n]; 	// key:공차, value: 등차수열 길이
+		for(int i = 0; i < n; i++) {
+			dp[i] = new HashMap<>();	// 초기화 / dp[i] -> arr[i]를 수열의 끝 원소로 하는 등차수열의미 
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				int diff = arr[i] - arr[j];		// 공차
+				int preLength = dp[j].getOrDefault(diff, 1);	// 최소 길이는 2부터 시작 / 이전값은 dp[j]
+				int curLength = preLength + 1;	// 현재 등차 수열 길이
 				
-				while(set.contains(nextNum)) {
-					curLength++;
-					nextNum += diff;
-				}
-				
+				dp[i].put(diff, Math.max(dp[i].getOrDefault(diff, 0), curLength));
 				maxLength = Math.max(maxLength, curLength);
 			}
 		}
