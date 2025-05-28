d8bb82a9ea6c502e5e418d5606373bbeb55479b0
skyungg <167261107+skyungg@users.noreply.github.com>
2025-05-28 03:05:01 +0000

    unsolved 등차수열

--- Baekjoon/등차수열/등차수열_허승경.java
+++ Baekjoon/등차수열/등차수열_허승경.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 /*
  * (범위 확인) -> arr[i] >= 0 && arr[i] < 10^9 -> int 형 으로 선언
+ * 아이디어 : arr[i]를 끝원소로 하는 등차수열  
+ * ---
  * 초기 아이디어: 이분탐색 -> 아닌듯 => 선형탐색? -> 시간초과 발생
  * 1. 배열에 저장 후, 오름차순 정렬하기
  * 2. 제일 작은 값과 큰 값의 차 구하기
@@ -13,33 +15,32 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int n = Integer.parseInt(br.readLine());
 		int [] arr = new int[n];
+		Set<Integer> set = new HashSet<>();
+		
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			set.add(arr[i]);
 		}
-		int maxLength = -1;	// 최대 길이
+		
 		Arrays.sort(arr); 	// 오름차순 정렬
-		int maxSub = arr[n-1] - arr[0];		// 길이 차이
+		int maxLength = 1;	// 최대 길이 초기화 (n의 최솟값 1)
 		
-		int common = 0;		// 공차
-		
-		while(common <= maxSub) {
-
-			for(int i = 0; i < n-1; i++) {
-				int preNum = arr[i];	// 이전 숫자
-				int curLen = 1;			// 현재 공차로 진행할 경우 수열의 길이// 이전 숫자
-				for(int j = i; j < n; j++) {
-					if(Math.abs(preNum-arr[j]) == common) {
-						preNum = arr[j];	// 이전 숫자 갱신
-						curLen++;
-					}
+		for(int i = 0; i < n-1; i++) {
+			for(int j = i+1; j < n; j++) {
+				int diff = arr[j]-arr[i];		// 앞-뒤 원소의 차
+				int curLength = 2;					// 원소의 길이(현재 2)
+				int nextNum = arr[j]+diff;		// 다음 원소 값
+				
+				while(set.contains(nextNum)) {
+					curLength++;
+					nextNum += diff;
 				}
-				maxLength = Math.max(maxLength, curLen);
-				if(maxLength == n) break;
+				
+				maxLength = Math.max(maxLength, curLength);
 			}
-			if(maxLength == n) break;
-			common++;
 		}
 		
 		System.out.println(maxLength);
